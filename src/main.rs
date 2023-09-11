e9975cb9a90ea1d0b46a86a244aac396201be99f
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-09-11 18:37:04 +0800

    Starting code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 9000.0,
+            range: 100.,
+            shadows_enabled: false,
+            ..default()
+        },
+        transform: Transform::from_xyz(8.5, 50.0, 8.0),
+        ..default()
+    });
 }
